13ee7b5c473e0f8e366712a2550be3d3fc3600b1
kpose <ganihujude@gmail.com>
2021-02-08 19:31:24 +0100

    make and get posts(functions)

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,6 +1,50 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello Lilplay!");
+admin.initializeApp();
+
+const express = require("express");
+
+const app = express();
+
+/* Get Posts */
+app.get("/posts", (req, res) => {
+  admin
+    .firestore()
+    .collection("posts")
+    .get()
+    .then((data) => {
+      let posts = [];
+      data.forEach((doc) => {
+        posts.push(doc.data());
+      });
+      return res.json(posts);
+    })
+    .catch((err) => console.error(err));
 });
+
+/* Create Posts */
+app.post("/post", (req, res) => {
+  const newPost = {
+    body: {
+      textBody: req.body.body.textBody,
+      audioBody: req.body.body.audioBody,
+    },
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+
+  admin
+    .firestore()
+    .collection("posts")
+    .add(newPost)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Something went wrong" });
+      console.error(err);
+    });
+});
+
+exports.api = functions.https.onRequest(app);
