8b804960b9938e5977842fafe2863f1776fd309c
kpose <ganihujude@gmail.com>
2021-02-08 22:38:39 +0100

    signup validation(functions)

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,22 +1,31 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const config = require("./firebaseConfig");
+const app = require("express")();
 
 admin.initializeApp();
 
 const express = require("express");
 
-const app = express();
+const firebase = require("firebase");
+firebase.initializeApp(config);
+
+const db = admin.firestore();
 
 /* Get Posts */
 app.get("/posts", (req, res) => {
-  admin
-    .firestore()
-    .collection("posts")
+  db.collection("posts")
+    .orderBy("createdAt", "desc")
     .get()
     .then((data) => {
       let posts = [];
       data.forEach((doc) => {
-        posts.push(doc.data());
+        posts.push({
+          postId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+        });
       });
       return res.json(posts);
     })
@@ -31,12 +40,10 @@ app.post("/post", (req, res) => {
       audioBody: req.body.body.audioBody,
     },
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    createdAt: new Date().toISOString(),
   };
 
-  admin
-    .firestore()
-    .collection("posts")
+  db.collection("posts")
     .add(newPost)
     .then((doc) => {
       res.json({ message: `document ${doc.id} created successfully` });
@@ -44,6 +51,81 @@ app.post("/post", (req, res) => {
     .catch((err) => {
       res.status(500).json({ error: "Something went wrong" });
       console.error(err);
+    });
+});
+
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+};
+
+const isEmpty = (string) => {
+  if (string.trim() === "") return true;
+  else return false;
+};
+
+/* signup route */
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+
+  let errors = {};
+
+  if (isEmpty(newUser.email)) {
+    errors.email = "Must not be empty";
+  } else if (!isEmail(newUser.email)) {
+    errors.email = "Must be a valid email address";
+  }
+
+  if (isEmpty(newUser.password)) errors.password = "Must not be empty";
+  if (newUser.password !== newUser.confirmPassword)
+    errors.confirmPassword = "Passwords must match";
+  if (isEmpty(newUser.handle)) errors.handle = "Must not be empty";
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+  /* TODO: vaidate data */
+
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: "This handle is already taken" });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCrendentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId: userId,
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCrendentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.log(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already in use" });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
     });
 });
 
---
+++ dbschema.js
@@ -0,0 +1,14 @@
+let db = {
+  posts: [
+    {
+      userHandle: "user",
+      body: {
+        textBody: "this is text body",
+        audioBody: "this is audio body",
+      },
+      createdAt: "2020-03-15T11:46:01.018Z",
+      likeCount: 5,
+      commentCount: 15,
+    },
+  ],
+};
