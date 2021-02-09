1dc287d797f4438ec158216baf57f39a5effd934
kpose <ganihujude@gmail.com>
2021-02-09 10:10:01 +0100

    restructure(functions)

---
+++ backend/functions/handlers/posts.js
@@ -0,0 +1,41 @@
+const { db } = require("../utils/admin");
+
+exports.getAllPosts = (req, res) => {
+  db.collection("posts")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then((data) => {
+      let posts = [];
+      data.forEach((doc) => {
+        posts.push({
+          postId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+        });
+      });
+      return res.json(posts);
+    })
+    .catch((err) => console.error(err));
+};
+
+exports.makeOnePost = (req, res) => {
+  const newPost = {
+    body: {
+      textBody: req.body.body.textBody,
+      audioBody: req.body.body.audioBody,
+    },
+    userHandle: req.user.handle,
+    createdAt: new Date().toISOString(),
+  };
+
+  db.collection("posts")
+    .add(newPost)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Something went wrong" });
+      console.error(err);
+    });
+};
